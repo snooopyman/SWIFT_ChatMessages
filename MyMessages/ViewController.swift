9ff366a6188f2e7de7009974f30af5c6657f72ed
jcv767 <jcv767@inlumine.ual.es>
2022-08-23 19:36:28 +0200

    Initial Commit

---
+++ MyMessages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMessages
+//
+//  Created by Armando Cáceres on 23/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
